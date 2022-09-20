e1a9e94feba256428694f4eb36015dfd096d8047
alexandrehdev <hallyssonrdev@gmail.com>
2022-09-20 09:30:15 -0300

    route

--- src/Path/Route.php
+++ src/Path/Route.php
@@ -4,9 +4,10 @@ use Guide\Comphass\Server\Response;
 
 class Route{
     
-    private static $error = "/error";
 
     private static $response = [];
+
+
 
 
     public static function buildResponse(string $path, callable $action){
@@ -23,11 +24,8 @@ class Route{
      * @return void
      */
     public static function error(){
-        $error = self::$error;
-
-        if($_SERVER["REQUEST_URI"] == $error){
-            (new Response)->getErrorPage();   
-        }
+        $response = new Response();
+        echo $response->getErrorPage();
     }
 
 
@@ -42,22 +40,16 @@ class Route{
      * @return void
      */
     public static function redirect(string $path, callable $action){
+      self::$response = self::buildResponse($path,$action);
+      $response = explode(",", self::$response["path"]);
 
-        if($path != $_SERVER["REQUEST_URI"]){
-            header("Location: /error");
-        }else{
-            self::$response = self::buildResponse($path,$action);
-            $response = explode(",", self::$response["path"]);
-
-            foreach($response as $result){
-               if($result == $_SERVER["REQUEST_URI"]){
-                 echo $action();
-             }
+        foreach($response as $result){
+            if($result == $_SERVER["REQUEST_URI"]){
+                echo $action();
+            }
         }
+
     }
-
-
-}
 
 
 
