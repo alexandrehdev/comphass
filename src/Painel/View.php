8f9163b466d6939a399c1ec241447d7582b293d4
alexandrehdev <hallyssonrdev@gmail.com>
2022-09-30 10:34:52 -0300

    changes get redirect methods

--- src/Painel/View.php
+++ src/Painel/View.php
@@ -9,20 +9,45 @@ class View{
     private $extension;
 
 
+    private $root;
+
 
     function __construct(){
+        $this->root = getcwd();
         $this->extension = ".painel.php";
         $this->resources = "/resources/views/";
     }
 
 
+    public function generateKeys(array $content) :array{
+        $keys = array_map(function($item){
+            return "{{".$item."}}";
+        },array_keys($content));  
+
+        return $keys;
+    }
+
+    public function responsePath(string $path, string $url, array $keys, array $vars, string $content) :void{
+        $values = array_values($vars);
+        echo ($path == $url) ? str_replace($keys,$values,$content) : ' ';
+    }
+
 
     public function exists(string $view){
-      if(file_exists(getcwd() . $this->resources . $view . $this->extension)){
-          return file_get_contents(getcwd() . $this->resources . $view . $this->extension);
-      }else{
-          return false;
-      }
+        $root = $this->root;
+        $resources = $this->resources;
+        $extension = $this->extension;
+
+        if(file_exists($root . $resources . $view . $extension))
+        {
+            $viewContent = file_get_contents($root . $resources . $view . $extension);
+
+            return $viewContent;
+        }
+        else{
+
+            return false;
+        }
     }
     
 }
